2655af560e972f7cad2dbbcb2913a0348668d7cd
jewel <github@thesteve.org>
2012-12-07 13:07:30 -0700

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheese::Application.config.secret_token = '8e3a49950268b594411d4daa5834a3b09873324e2cb012218e0cfb7f3e38e1e973cad83d9e1748e7e01eb4bf312f3b8eabc14eb834df44f8ede73cdb2acbfcbb'
