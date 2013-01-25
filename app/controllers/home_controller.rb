e21389605bb5fda979bdf4da27ee59bdcd77a51b
jewel <github@thesteve.org>
2013-01-25 16:38:20 -0700

    Show first 100 events on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,18 @@
 class HomeController < ApplicationController
   def index
     @by_event = Hash.new { |h,k| h[k] = [] }
-    Item.order('taken desc').limit(100).each do |item|
-      @by_event[item.event].push item
+    pos = 0
+    while @by_event.size < 100
+      res = Item.order('taken desc').offset(pos).limit(1000)
+      need_count = true
+      res.each do |item|
+        need_count = false
+        @by_event[item.event].push item
+      end
+      if need_count
+        break if res.count == 0
+      end
+      pos += 1000
     end
   end
 end
