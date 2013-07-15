3ca4bef0990d5b8b2805266e3b1622015cf35266
jewel <github@thesteve.org>
2013-07-14 23:09:18 -0600

    Fix facebook oauth

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_filter :verify_approval!
 
   def facebook
-    @user = User.find_for_facebook_auth(request.env['omniauth.auth'], current_user)
+    @user = User.find_for_facebook_oauth(request.env['omniauth.auth'], current_user)
 
     sign_in_and_redirect @user, event: :authentication
     set_flash_message(:notice, :success, kind: "Facebook")
