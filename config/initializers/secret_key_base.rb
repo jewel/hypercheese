81a52662d6e8484479fcbcc1fd57dc55e4319d86
jewel <github@thesteve.org>
2013-07-14 23:25:26 -0600

    Fix secret key behavior

---
+++ config/initializers/secret_key_base.rb
@@ -0,0 +1,7 @@
+path = "#{Rails.root}/.secret_key_base"
+
+if !File.exists?(path)
+  raise "Missing secret key.  Create it by running 'rake secret > #{path}'"
+end
+
+HyperCheese::Application.config.secret_key_base = File.read(path).chomp
