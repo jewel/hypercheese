2d9e2a15907f68a070b96c2d4bac6035ce434c01
jewel <github@thesteve.org>
2015-10-28 19:07:11 -0600

    Exclude comment ids from serialized items

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,9 +2,8 @@ class ItemSerializer < ActiveModel::Serializer
   embed :ids
   attributes :id, :taken, :width, :height, :has_comments, :variety
   has_many :tags
-  has_many :comments
 
   def has_comments
-    comments.any?
+    object.comments.any?
   end
 end
