3767ffe9aaaec30ddae14a9799ca67ceba40c40d
jewel <github@thesteve.org>
2015-10-31 22:04:27 -0600

    Remove information we no longer use

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer < ActiveModel::Serializer
   embed :ids
-  attributes :id, :taken, :width, :height, :has_comments, :variety
+  attributes :id, :taken, :has_comments, :variety
   has_many :tags
 
   def has_comments
