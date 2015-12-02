72e20ac5603b987f6bb515731ba1be99bbd9523f
jewel <github@thesteve.org>
2015-12-01 21:52:13 -0700

    Set session timeout to be 10 years

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-HyperCheese::Application.config.session_store :cookie_store, key: '_hyper_cheese_session'
+HyperCheese::Application.config.session_store :cookie_store, key: '_hyper_cheese_session', expire_after: 60 * 60 * 24 * 365 * 10
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
