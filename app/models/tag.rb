50ad19eeb54f28a136d6f0064d758c6663246fe5
jewel <github@thesteve.org>
2016-01-10 16:51:04 -0700

    Maintenance scripts

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   belongs_to :icon, class_name: 'Item', foreign_key: "icon_item_id"
-  has_many :items
+  has_many :item_tags
+  has_many :items, through: :item_tags
 end
