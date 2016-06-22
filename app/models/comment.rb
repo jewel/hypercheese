b1f36f6030f3e06ae241ceecc1f2f1d248d56e3d
jewel <github@thesteve.org>
2016-06-21 21:13:06 -0600

    Cache home page content, update when necessary

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
+
+  after_save do |comment|
+    UpdateActivityJob.perform_later
+  end
 end
--- app/models/star.rb
+++ app/models/star.rb
@@ -1,4 +1,8 @@
 class Star < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
+
+  after_save do |comment|
+    UpdateActivityJob.perform_later
+  end
 end
