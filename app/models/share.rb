0b572ebf16f653bf76ea3c8d7a29ec4bb252a846
jewel <github@thesteve.org>
2021-10-26 17:14:08 -0600

    Hide deleted items in shares
    Usually deleted items won't show up in shares because they can't be
    selected to add them to the share.  This doesn't enforce the deleted
    tag, just the deleted field for an item that has no files.

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,5 +1,5 @@
 class Share < ActiveRecord::Base
   has_many :share_items
-  has_many :items, through: :share_items
+  has_many :items, -> { where( deleted: false ) }, through: :share_items
   belongs_to :user
 end
